eca91933e69d1f0e6f6d03473a60ce642d085575
deepencoding <deepanshugautam12a13@gmail.com>
2023-08-25 01:04:07 +0530

    Add project files.

---
+++ IndeXpress/IndeXpress.cpp
@@ -0,0 +1,84 @@
+#include "Synchronizer.hpp"
+#include "WorkerThread.hpp"
+#include "Searcher.hpp"
+
+#include <iostream>
+#include <string>
+#include <map>
+#include <iomanip>
+#include <filesystem>
+#include <Windows.h>
+
+namespace fs = std::filesystem;
+
+// Number of processor threads
+const unsigned int WThreadCount = 3;
+
+// flag indicating if search of .txt file is completed by SearchThread
+bool SearchComplete = false;
+
+// Synchronizer queue object
+Synchronizer SyncQ;
+
+// C:\Dev\Projects\IndeXpress\IndeXpress\x64\Debug
+// issue : D:\a\_work\1\s\src\vctools\crt\github\stl\src\mutex.cpp(61): mutex destroyed while busy
+int main()
+{
+	std::string dir;
+
+	std::cout << "Please Specify the Directory Path: ";
+
+	std::cin >> dir;
+	const fs::path inputPath = dir;
+	
+	if (!fs::exists(inputPath))
+	{
+		std::cout << "\nDirectory doesn't exist. Exiting Code.\n";
+		return -1;
+	}
+
+	DWORD p_id = GetCurrentProcessId();
+
+	std::cout << "\nPlease wait while process(" << p_id << ")" << " is processing....\n";
+
+	// Creating Worker Threads
+	WorkerThread WThread(WThreadCount);
+	WThread.m_CreateWorkerThreads();
+
+	// Creating Search thread to add .txt files in Queue
+	Searcher SearchThObj;
+	SearchThObj.m_StartSearch(dir);
+
+
+	while (1)
+	{
+		if (SearchComplete && !(SyncQ.m_GetFileCount()))
+			break;
+	}
+
+	std::cout << "Search of all txt files completed!\n";
+
+	std::multimap <int, std::string, std::greater<int> > MTable;
+	MTable = WThread.m_GetTableEntry();
+
+	std::cout << "\nTotal files Processed " << SyncQ.m_GetProcessedFileCount() << std::endl;
+	std::cout << "***********************************************\n";
+	std::cout << " " << std::setw(10) << "Words" << std::setw(20) << "No of occurences\n";
+	std::cout << "***********************************************\n";
+
+	int i = 0;
+	for (auto iter = MTable.begin(); iter != MTable.end(); iter++)
+	{
+		std::cout << " " << std::setw(10) << iter->second << std::setw(10) << iter->first << "\n";
+		i++;
+
+		// To get only Top 10 words
+		if (i == 10)
+			break;
+	}
+
+	std::cout << "***********************************************\n";
+
+	return 0;
+
+}
---
+++ IndeXpress/WorkerThread.cpp
@@ -0,0 +1,106 @@
+#include "WorkerThread.hpp"
+
+#include <thread>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <chrono>
+#include <utility>
+#include <filesystem>
+
+namespace fs = std::filesystem;
+
+extern Synchronizer SyncQ;
+
+//Constructor
+WorkerThread::WorkerThread(int WThCnt)
+	: m_WorkerThreadCount(WThCnt)
+{
+}
+
+// Destructor
+WorkerThread::~WorkerThread()
+{
+}
+
+void WorkerThread::m_CreateWorkerThreads()
+{
+	// Create m_WorkerThreadCount worker threads by looping that times
+	for (int i = 0; i < m_WorkerThreadCount; i++)
+	{
+		std::thread t(&WorkerThread::m_FillTable, this);
+		t.detach();
+		// threads.push_back(std::move(t));
+	}
+
+}
+
+void WorkerThread::m_AddWordInTable(std::string word)
+{
+	// Insert word into Table if not present
+	// If present then increase count by 1
+	m_WThMutexObj.lock();
+	auto it = Table.find(word);
+	if (it == Table.end())
+	{
+		Table[word]++;
+	}
+	else
+	{
+		(it->second)++;
+	}
+	m_WThMutexObj.unlock();
+
+}
+
+void WorkerThread::m_FillTable()
+{
+	while (1)
+	{
+		std::string filename = SyncQ.m_GetFrontFile();
+		if (!(filename.empty()))
+		{
+			std::ifstream file;
+			file.open(filename.c_str(), std::ifstream::in);
+			std::string line;
+
+			if (file.is_open())
+			{
+				while (std::getline(file, line))
+				{
+					std::size_t prev = 0, pos{};
+					while ((pos = line.find_first_of("~`=!@#$%^&*)/\?-_|[,. }](_-+{;':\"></", prev)) != std::string::npos)
+					{
+						if (pos > prev)
+						{
+							m_AddWordInTable(line.substr(prev, pos - prev));
+						}
+						prev = pos + 1;
+					}
+
+					if (prev < line.length())
+					{
+						m_AddWordInTable(line.substr(prev, std::string::npos));
+					}
+				}
+
+				file.close();
+			}
+			else
+			{
+				std::cout << "--Unable to Open File--: " << filename << "\n";
+			}
+		}
+	}
+}
+
+std::multimap <int, std::string, std::greater<int> > WorkerThread::m_GetTableEntry()
+{
+	std::multimap <int, std::string, std::greater<int> > m_table;
+	auto iter = Table.begin();
+	for (; iter != Table.end(); iter++)
+	{
+		m_table.insert(std::make_pair(iter->second, iter->first));
+	}
+	return m_table;
+}
